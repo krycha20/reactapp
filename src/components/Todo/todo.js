7b93620f826ff3953045f391300082d3ef87814b
Krycha20 <krystian.gimlik@outlook.com>
2021-09-21 23:29:16 +0200

    add api Rest connect, function addToDo, removeTodo

--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -1,80 +1,99 @@
-import React, { useState } from 'react';
-import './todo.scss'
+import React, { Component } from 'react';
+import './todo.scss';
 import TodoItem from './todoItem';
 import NewTodoForm from './newTodoForm';
+import * as toDoItemApi from '../../helpers/toDoItemApi';
+import * as _ from 'ramda';
 
-class Todo extends React.Component {
-  state = {
-    tasksList: [
-      {
-        id: '1',
-        title: 'zrobic zakupy',
-        isCompleted: true
-      },
-      {
-        id: '2',
-        title: 'frachty',
-        isCompleted: false
-      }
-    ],
-    inputValue: ''
+class Todo extends Component {
+  
+  componentDidMount = async () => {
+    const tasks = await toDoItemApi.getAll()
+    this.setState({ tasks })
+
+    toDoItemApi.getAll()
   }
 
-  toggleCheck(id) {
-    const index = this.state.tasksList.findIndex(x => x.id === id)
-    const newElements = this.state.tasksList
-    newElements[index].isCompleted = !newElements[index].isCompleted
+  state = {
+    tasks: [],
+    inputValue: ''
+  }
+  
+  toggleCheck = (id) => {
+    console.log('id => ', id)
+    // fetch(`http://localhost:5000/tasksList/${id}`, {
+    //   method: 'PUT',
+    //   headers: { "Content-Type": 'application/json; charset=utf-8' },
+    //   body: JSON.stringify({
+    //     done: !this.state.done
+    //   })
+    // }).then(response => {
+    //   if(response.ok) {
+    //     this.setState({ done: !this.state.done} )
+    //   }
+    // });
+    // const index = this.state.tasksList.findIndex(x => x.id === id)
+    // const newElements = this.state.tasksList
+    // newElements[index].isCompleted = !newElements[index].isCompleted
 
-    this.setState({ tasksList: newElements })
+    // this.setState({ tasksList: newElements })
   }
 
   updateDraft = event => {
     this.setState({ inputValue: event.target.value })
   }
 
-  addTodo = () => {
-    // add to list
-    let checkInput = false;
-    const item = {
-      id: Math.random(),
-      title: this.state.inputValue
-    }
-
-    const newElements = [item, ...this.state.tasksList];
-
-    if(item.title === '') {
-      checkInput = true;
-    } else {
-      this.setState({ tasksList: newElements })
-      this.setState({ inputValue: '' })
-    }
+  addTodo = async () => {
+    const { tasks, inputValue } = this.state;
+    const task = await toDoItemApi.create({ title: inputValue, done: false});
+    this.setState({ tasks: _.append(task, tasks), inputeValue: ''})
   }
 
-  removeTodo(id) {
-    const newList = this.state.tasksList.filter((item) => item.id !== id)
-    this.setState({ tasksList: newList })
+  findById = (id, arr) => {
+    const index = _.findIndex(_.propEq('id', id))(arr)
+
+    return { index, task : arr[index]}
   }
 
-  removeAll = () => {
-    console.log('dsadsa', this.state.tasksList);
-    this.setState({tasksList: []});
+  destroyTodo = async (id) => {
+    const { tasks} = this.state;
+    await toDoItemApi.destroy(id)
+    const { index } = this.findById(id, tasks);
+
+    this.setState({ tasks: _.remove(index, 1, tasks) });
+  }
+
+  removeAll = async () => {
+    const { tasks} = this.state;
+    await toDoItemApi.destroy(tasks)
+    console.log('remove all', tasks);
+    this.setState({ tasks: _.remove(tasks)} );
   }
 
   editTodo() {
   }
 
   render() {
-    const { tasksList, inputValue } = this.state;
-    const tasks = tasksList.map(task => {
-      console.log('task', task);
-      return <TodoItem task={task} markClicked={this.toggleCheck.bind(this)} removeHandler={this.removeTodo.bind(this)} editHandler={this.editTodo.bind(this)} />
+    const { tasks, inputValue } = this.state;
+    const taskItem = tasks.map(item => {
+      return <TodoItem 
+        id={item.id} 
+        key={item.id} 
+        task={item} 
+        // markClicked={this.toggleCheck.bind(this)} 
+        destroy={this.destroyTodo} 
+        />
     })
 
-    console.log('taksk', tasks)
+    let otherOptions;
+    if(tasks.length)
+      otherOptions = 
+        <div className="other-actions">
+          <button onClick={this.removeAll}>Remove All</button>
+        </div>;
 
     return (
       <div className="todo-container">
-        {tasks ? 'elo' : 'nie elo'}
         <h1 className="title">Todo - app</h1>
         <div className="card-container mb-20">
           <NewTodoForm 
@@ -83,12 +102,8 @@ class Todo extends React.Component {
             inputValue={inputValue}
             />
         </div>
-        <div className="other-actions">
-          <button onClick={this.removeAll}>Remove All</button>
-        </div>
-        <div className="card-container tasks-list">
-          {tasks}
-        </div>
+        {otherOptions}
+        <div className="card-container tasks-list">{taskItem}</div>
       </div>
     )
   }
--- src/components/Todo/todoItem.js
+++ src/components/Todo/todoItem.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const TodoItem = props => {
-  const title = props.task.title;
-  const done = props.task.isCompleted;
+class TodoItem extends Component {
   
-  return (
-    <div className={`card${done ? ' completed' : '' }`} >
-      <div className="card-content">
-        <span className={`check-button${done ? ' checked' : '' }`} onClick={() => props.markClicked(props.task.id)}></span>
-        <h2>{title}</h2>
+  destroy = () => this.props.destroy(this.props.id);
+  
+  render () {
+    const props = this.props;
+    const title = props.task.title;
+    const done = props.task.done;
+  
+    return (
+      <div className={`card${done ? ' completed' : '' }`} >
+        <div className="card-content">
+          <span className={`check-button${done ? ' checked' : '' }`} onClick={() => props.markClicked(props.task.id)}></span>
+          <h2>{title}</h2>
+        </div>
+        <div className="action-buttons">
+          <span className="button edit-button" 
+            // onClick={() => props.editHandler(props.task.id, title)}
+            >
+            <FontAwesomeIcon icon="edit" />
+          </span>
+          <span className="button remove-button"
+            onClick={this.destroy}
+            >
+            <FontAwesomeIcon icon="trash-alt" />
+          </span>
+        </div>
       </div>
-      <div className="action-buttons">
-        <span className="button edit-button" onClick={() => props.editHandler(props.task.id, title)}>
-          <FontAwesomeIcon icon="edit" />
-        </span>
-        <span className="button remove-button" onClick={() => props.removeHandler(props.task.id)}>
-          <FontAwesomeIcon icon="trash-alt" />
-        </span>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TodoItem;
